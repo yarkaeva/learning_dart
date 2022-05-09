3221a0a49868998917b375f3028ee022201b80f0
yarkaeva <darina.yarkaeva@gmail.com>
2022-05-09 14:48:37 +0500

    Урок по сеттерам и геттерам.

---
+++ Уроки/07-объектно-ориентированное_програмирование/007-сеттеры_и_геттеры.dart
@@ -0,0 +1,21 @@
+void main() {
+  var myCar = Car();
+  myCar.carName = 'Ford'; // используется "сеттер" по умолчанию
+  print(myCar.carName); // используется "геттер" по умолчанию
+
+  myCar.percentage = 23.5;//вызываем собственный "сеттер"
+  print(myCar.percentage);//вызываем собственный "геттер"
+}
+
+class Car {
+  late String carName;
+  late double _percentWay; //приватное значение
+
+  void set percentage(double per) {
+    _percentWay = (per > 100 || per < 0) ? _percentWay = 0 : _percentWay = per;
+  }
+
+  double get percentage {
+    return _percentWay;
+  }
+}
