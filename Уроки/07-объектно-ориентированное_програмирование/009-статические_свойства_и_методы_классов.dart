a317c0d87f1c997376af562d6669f3d3a5c9e4d4
yarkaeva <darina.yarkaeva@gmail.com>
2022-05-10 21:46:29 +0500

    Изучила урок по статических членам классов.

---
+++ Уроки/07-объектно-ориентированное_програмирование/009-статические_свойства_и_методы_классов.dart
@@ -0,0 +1,41 @@
+//Преимущества статических полей и методов перед нестатическими заключается в меньшем потреблении памяти.
+//Статические поля и методы не инициализируются до того момента, пока они не начнут использоваться в программе.
+//Значения статических полей, методов и констант относятся в целом ко всему классу, а не к отдельным объектам(его экземплярам).
+
+void main() {
+  Car car = Car('Daewoo Nexia', 'red');
+  //  Car.wheels = 4; //если мы хотим обратиться к полю static вне класса, то нужно использовать имя класса. Лучше так не делать.
+  Car.setWheels(3);
+  car.checkmove();
+}
+
+class Car {
+  String carName;
+  String color;
+  static int wheels = 3;
+  static final int wheels2 = 3; //или const, свойство можно определить как статическую константу
+
+  Car(this.carName, this.color) {
+    var t = time(500, 80);
+    print('Time: $t');
+  }
+
+  static double time(int dist, double speed) =>dist / speed; //как правило статические методы выполняют такие вычисления, которые не затрагивают состояние или поведение объекта
+
+  static void setWheels(int value) {
+    // в статических методах мы не можем использовать нестатические методы и свойства классов
+    if (value == 4) {
+      wheels = value;
+    } else {
+      print('Please,correct value(4)');
+    }
+  }
+
+  void checkmove() {
+    if (wheels < 4) {
+      print('The $carName is broken');
+    } else {
+      print('The $color $carName has started');
+    }
+  }
+}
